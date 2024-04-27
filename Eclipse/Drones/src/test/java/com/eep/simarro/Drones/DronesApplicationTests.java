84e008af20cb50839d7b1805a26a6aef058cecda
Alejandro-Simarro <145431434+Alejandro-Simarro@users.noreply.github.com>
2024-04-27 16:57:04 +0200

    Creo maven

---
+++ Eclipse/Drones/src/test/java/com/eep/simarro/Drones/DronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eep.simarro.Drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
