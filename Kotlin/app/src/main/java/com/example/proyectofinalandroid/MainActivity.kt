a98707963ece1b1aa6f9bfe22ab35f8fe288fa8a
Alejandro-Simarro <145431434+Alejandro-Simarro@users.noreply.github.com>
2024-05-15 12:15:41 +0200

    Empezamos de 0

--- Kotlin/app/src/main/java/com/example/proyectofinalandroid/MainActivity.kt
+++ Kotlin/app/src/main/java/com/example/proyectofinalandroid/MainActivity.kt
@@ -1,29 +1,28 @@
-package com.example.proyectoandroid
+package com.example.proyectofinalandroid
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import android.os.Bundle
-import androidx.compose.material.Text
-import com.example.proyectoandroid.ui.theme.ProyectoAndroidTheme
+import com.example.proyectofinalandroid.ui.theme.ProyectoFinalAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProyectoAndroidTheme {
+            ProyectoFinalAndroidTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Aquí se muestra la interfaz de formulario de drones
-                    DronesFormulario(onGuardarClick = { /* Lógica para guardar el dron */ })
+                    Greeting("Android")
                 }
             }
         }
@@ -31,17 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DronesFormulario(onGuardarClick: () -> Unit) {
-    // Aquí va el código del formulario de drones
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ProyectoAndroidTheme {
-        // Ejemplo de un texto dentro de la vista previa
-        Surface {
-            Text("Hola desde la vista previa")
-        }
+    ProyectoFinalAndroidTheme {
+        Greeting("Android")
     }
 }
