f320b612badf7f9c5c82d28ece70a9c0a0b09bba
Alejandro-Simarro <145431434+Alejandro-Simarro@users.noreply.github.com>
2024-05-16 15:35:52 +0200

    desistalo android

--- AndroidDrones/app/src/main/java/com/example/aplicacionfinaldrones/Drone.kt
+++
@@ -1,8 +0,0 @@
-package com.example.aplicacionfinaldrones
-
-data class Drone(
-    val id: Int,
-    val name: String,
-    val apellido: String,
-    val de: String
-)
--- AndroidDrones/app/src/main/java/com/example/aplicacionfinaldrones/DroneApiService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.aplicacionfinaldrones
-
-import retrofit2.http.GET
-
-interface DroneApiService {
-    @GET("drones")
-    suspend fun getDrones(): List<Drone>
-}
