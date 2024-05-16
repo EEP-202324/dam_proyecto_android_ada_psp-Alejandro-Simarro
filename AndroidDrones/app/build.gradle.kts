0eb32f1c990d9644f780f8f01a6a71b77291e3cf
Alejandro-Simarro <145431434+Alejandro-Simarro@users.noreply.github.com>
2024-05-16 17:08:03 +0200

    Creando dependencias

--- AndroidDrones/app/build.gradle.kts
+++ AndroidDrones/app/build.gradle.kts
@@ -66,4 +66,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // Retrofit para comunicación de red
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Kotlin Coroutines para trabajo asíncrono
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
 }
